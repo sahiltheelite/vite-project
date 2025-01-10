024b35faed7be29c278bbbdc99a44234e6e96f13
Sahil Arshad <sahilthebest078@gmail.com>
2025-01-11 00:16:19 +0500

    add revert console log

--- src/components/counter/Contact.jsx
+++ src/components/counter/Contact.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 
 const Contact = () => {
+  console.log("hi github")
+  console.log("hello github")
+
   return (
     
     <div>All Contact
-      <h1>Hi branch concept{console.log("hi")}</h1>
+      <h1>Hi branch concept{console.log("Trying out revert")}{}</h1>
     </div>
   )
 }
