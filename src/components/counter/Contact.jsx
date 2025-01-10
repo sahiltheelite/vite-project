9f9122ec1ab99fa5b2767b5dcb769d8027938790
Sahil Arshad <sahilthebest078@gmail.com>
2025-01-10 22:32:08 +0500

    Added branch heading in contact

--- src/components/counter/Contact.jsx
+++ src/components/counter/Contact.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>All Contact</div>
+    
+    <div>All Contact
+      <h1>Hi branch concept{console.log("hi")}</h1>
+    </div>
   )
 }
 
